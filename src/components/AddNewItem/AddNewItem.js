e4d95ac6d5bd6912c01f904896ff5d63a363f7bb
smfahim25 <smfahimhossen@gmail.com>
2022-05-01 02:19:07 +0600

    new item section

---
+++ src/components/AddNewItem/AddNewItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddNewItem = () => {
+    return (
+        <div>
+            <h2>Add new item Page</h2>
+        </div>
+    );
+};
+
+export default AddNewItem;
