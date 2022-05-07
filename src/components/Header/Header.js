9a5bd463fd6664a771cbb40b9e31757cf1af40a4
smfahim25 <smfahimhossen@gmail.com>
2022-05-07 14:26:58 +0600

    header changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Header = () => {
+    // Sign out process 
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='bg-black'>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand as={Link} to={"/home"}>Bike House</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -14,12 +23,32 @@ const Header = () => {
 
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to={"/home"} className='text-white'>Home</Nav.Link>
-                            <Nav.Link as={Link} to={"/manage-inventory"} className='text-white'>Manage Item</Nav.Link>
-                            <Nav.Link as={Link} to={"/my-item"} className='text-white'>My Item</Nav.Link>
-                            <Nav.Link as={Link} to={"/blog"} className='text-white'>Blog</Nav.Link>
-                            <Nav.Link as={Link} to={"/login"} className='text-white'>Login</Nav.Link>
-                            <Nav.Link as={Link} to={"/signup"} className='text-white'>Register</Nav.Link>
+                            <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+
+
+                            <Nav.Link as={Link} to={"/blog"}>Blog</Nav.Link>
+                            {
+                                user ? <Nav.Link as={Link} to={"/my-item"}>My Bike</Nav.Link>
+
+                                    : ''
+                            }
+                            {
+                                user ? <Nav.Link as={Link} to={"/manage-inventory"}>Manage Bikes</Nav.Link>
+
+                                    : ''
+                            }
+                            {
+                                user ? <Nav.Link as={Link} to={"/add-new"}>Add New Bike</Nav.Link>
+
+                                    : ''
+                            }
+                            {
+                                user ? <Nav.Link onClick={handleSignOut} >Logout</Nav.Link>
+
+                                    : <Nav.Link as={Link} to={"/login"}>Login</Nav.Link>
+                            }
+
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
