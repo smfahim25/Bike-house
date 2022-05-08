e864eb3fccc04130f4131d712907cfb0922508de
smfahim25 <smfahimhossen@gmail.com>
2022-05-08 22:55:04 +0600

    banner code changed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,26 +14,26 @@ const Header = () => {
     }
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="danger">
                 <Container>
-                    <Navbar.Brand as={Link} to={"/home"}>Bike House</Navbar.Brand>
+                    <Navbar.Brand as={Link} to={"/home"} className='text-white'>Bike House</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
 
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+                            <Nav.Link as={Link} to={"/home"} className='text-white'>Home</Nav.Link>
 
 
-                            <Nav.Link as={Link} to={"/blog"}>Blog</Nav.Link>
+                            <Nav.Link as={Link} to={"/blog"} className='text-white'>Blog</Nav.Link>
                             {
-                                user ? <Nav.Link as={Link} to={"/my-item"}>My Bikes</Nav.Link>
+                                user ? <Nav.Link as={Link} to={"/my-item"} className='text-white'>My Bikes</Nav.Link>
 
                                     : ''
                             }
                             {
-                                user ? <Nav.Link as={Link} to={"/manage-inventory"}>Manage Bikes</Nav.Link>
+                                user ? <Nav.Link as={Link} to={"/manage-inventory"} className='text-white'>Manage Bikes</Nav.Link>
 
                                     : ''
                             }
@@ -43,9 +43,9 @@ const Header = () => {
                                     : ''
                             }
                             {
-                                user ? <Nav.Link onClick={handleSignOut} >Logout</Nav.Link>
+                                user ? <Nav.Link onClick={handleSignOut} className='text-white'>Logout</Nav.Link>
 
-                                    : <Nav.Link as={Link} to={"/login"}>Login</Nav.Link>
+                                    : <Nav.Link as={Link} to={"/login"} className='text-white'>Login</Nav.Link>
                             }
 
 
